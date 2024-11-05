d659acbb25645ba29411847192aa464365fa56e6
Ali <dev.alisevinc@gmail.com>
2024-11-05 22:27:46 +0300

    customize chart legend

--- src/components/layout/overview/OverviewBudget.tsx
+++ src/components/layout/overview/OverviewBudget.tsx
@@ -12,8 +12,30 @@ import { Budget } from "../../../lib/types";
 
 const COLORS = ["#3f3f46", "#b91c1c", "#4d7c0f", "#0369a1"];
 
+function legendContent(payload: Budget[]) {
+  return (
+    <ul className={styles.legend}>
+      {payload.map((entry, index) => (
+        <li
+          style={{ color: COLORS[index % COLORS.length] }}
+          key={`item-${index}`}
+        >
+          <div
+            style={{ backgroundColor: COLORS[index % COLORS.length] }}
+            className={styles.leftBar}
+          />
+          <span>
+            {entry.name.replace("_", " ").toUpperCase()} - ${entry.value}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default function OverviewBudget({ data }: { data: Budget[] }) {
   console.log(data);
+
   return (
     <div className={styles.budget}>
       <ResponsiveContainer width="100%" height="100%">
@@ -35,7 +57,12 @@ export default function OverviewBudget({ data }: { data: Budget[] }) {
               />
             ))}
           </Pie>
-          <Legend verticalAlign="top" iconType="circle" align="center" />
+          <Legend
+            verticalAlign="top"
+            iconType="circle"
+            align="center"
+            content={() => legendContent(data)}
+          />
           <Tooltip />
         </PieChart>
       </ResponsiveContainer>
